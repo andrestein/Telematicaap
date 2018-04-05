f9423058d3237685b1ef50fd47b981bf7e39a0d9
andrestein <andresgrandapiedrahita@gmail.com>
2018-04-04 22:17:00 -0500

    Backend creado y script creado e implementado

---
+++ Project/src/main/java/core/Config.java
@@ -0,0 +1,10 @@
+package core;
+
+public class Config {
+	
+	public static final String CHARSET = "UTF-8";
+	 public static String pathFile = "C:\\Users\\LENOVO\\Documents\\2018-1\\Telematica\\Proyecto\\project-web\\result//";
+	 public static String pathFileResult ="C:\\Users\\LENOVO\\Documents\\2018-1\\Telematica\\Proyecto\\project-web\\result//";
+	 public static String pathterraform ="C:\\Users\\LENOVO\\Documents\\2018-1\\Telematica\\Proyecto\\project-web\\result\\terraform.exe";
+
+}
